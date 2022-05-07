74b4fe65c0d2a1dcefd2061ea3ecaa5582cf38ef
Lixing <slxtrjh325@gmail.com>
2022-05-07 15:52:29 +0800

    Add summoner name

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,27 @@ import styles from "../styles/Home.module.css";
 import { scroller } from "../animation/scroller";
 import { useEffect } from "react";
 
+type individualPositions = "TOP" | "JUNGLE" | "MIDDLE" | "BOTTOM" | "UTILITY";
+
+const positionPriority: individualPositions[] = [
+  "TOP",
+  "JUNGLE",
+  "MIDDLE",
+  "BOTTOM",
+  "UTILITY",
+];
+
+const championImageSize = 32;
+const championImageSizeWithInterval = championImageSize + 8;
+
 interface IHero {
   id: number;
-  name: string;
+  championName: string;
+  summonerName: string;
+  totalDamageDealtToChampions: number;
+  totalDamageTaken: number;
+  position: individualPositions;
+  teamId: number;
 }
 
 interface HomeProps {
@@ -16,51 +34,92 @@ interface HomeProps {
 }
 
 const setupVisualization = (heroes: IHero[]) => {
-  d3.select("#visualization")
-  .selectAll("image")
-  .data(heroes)
-  .join("image")
-  .attr(
-    "href",
-    (d) =>
-      `http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/${d.name}.png`
-  )
-  .attr("height", 32)
-  .attr("width", 32)
-  .attr("x", 0)
-  .attr("y", (d, i) => i * 40);
+  let visualization = d3.select("#visualization");
+
+  visualization
+    .selectAll("image")
+    .data(heroes)
+    .join("image")
+    .attr(
+      "href",
+      (d) =>
+        `http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/${d.championName}.png`
+    )
+    .attr("height", championImageSize)
+    .attr("width", championImageSize)
+    .attr("x", 0)
+    .attr("y", (d, i) => i * (championImageSize + 8));
+
+  visualization
+    .selectAll("text.summonerName")
+    .data(heroes)
+    .join("text")
+    .attr("class", "summonerName")
+    .text((d) => d.summonerName)
+    .attr("x", 40)
+    .attr("y", (d, i) => i * (championImageSize + 8) + 20);
 };
 
 const playerSectionAnimation = (heroes: IHero[]) => {
-  d3.select("#visualization")
+  let visualization = d3.select("#visualization");
+
+  visualization
     .selectAll("image")
     .data(heroes)
     .join("image")
     .transition()
     .attr("x", 0)
-    .attr("y", (d, i) => i * 40);
+    .attr("y", (d, i) => i * championImageSizeWithInterval);
+
+  visualization
+    .selectAll("text.summonerName")
+    .data(heroes)
+    .join("text")
+    .transition()
+    .attr("x", championImageSizeWithInterval)
+    .attr(
+      "y",
+      (d, i) =>
+        i * championImageSizeWithInterval + championImageSizeWithInterval / 2
+    );
 };
 
 const teamSectionAnimation = (heroes: IHero[]) => {
-  d3.select("#visualization")
+  let visualization = d3.select("#visualization");
+
+  visualization
     .selectAll("image")
     .data(heroes)
     .join("image")
     .transition()
-    .attr("x", (d, i) => (i % 5) * 40)
-    .attr("y", (d, i) => i < 5 ? 0 : 200);
+    .attr("x", (d, i) => (i % 5) * 120)
+    .attr("y", (d, i) => (i < 5 ? 0 : 200));
+
+  visualization
+    .selectAll("text.summonerName")
+    .data(heroes)
+    .join("text")
+    .transition()
+    .attr("x", (d, i) => (i % 5) * 120)
+    .attr("y", (d, i) =>
+      i < 5
+        ? championImageSizeWithInterval + championImageSizeWithInterval / 2
+        : 200 +
+          championImageSizeWithInterval +
+          championImageSizeWithInterval / 2
+    );
 };
 
 const activationFunctions = [playerSectionAnimation, teamSectionAnimation];
 
 const Home: NextPage<HomeProps> = ({ heroes }) => {
   useEffect(() => {
+    let lastIndex: number;
+    let activeIndex = 0;
+
     let scroll = scroller().container(d3.select("body"));
 
     scroll();
-
-    let lastIndex: number;
-    let activeIndex = 0;
 
     setupVisualization(heroes);
 
@@ -116,7 +175,12 @@ const Home: NextPage<HomeProps> = ({ heroes }) => {
         </div>
       </div>
 
-      <svg id="visualization" className={styles.visualization} width="600" height="400"></svg>
+      <svg
+        id="visualization"
+        className={styles.visualization}
+        width="600"
+        height="400"
+      ></svg>
 
       <footer className={styles.footer}>
         <a
@@ -132,10 +196,27 @@ const Home: NextPage<HomeProps> = ({ heroes }) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const heroes: IHero[] = matchData.info.participants.map((entity) => ({
-    id: entity.championId,
-    name: entity.championName,
-  }));
+  const heroes: IHero[] = matchData.info.participants
+    .map((entity) => ({
+      id: entity.championId,
+      championName: entity.championName,
+      summonerName: entity.summonerName,
+      totalDamageDealtToChampions: entity.totalDamageDealtToChampions,
+      totalDamageTaken: entity.totalDamageTaken,
+      position: entity.individualPosition as individualPositions,
+      teamId: entity.teamId,
+    }))
+    .sort((a, b) => {
+      if (a.teamId < b.teamId) {
+        return -1;
+      } else if (a.teamId > b.teamId) {
+        return 1;
+      } else {
+        let positionA = positionPriority.indexOf(a.position);
+        let positionB = positionPriority.indexOf(b.position);
+        return positionA < positionB ? -1 : 1;
+      }
+    });
 
   return {
     props: {
